35c4852c0d9cb915d0632105935ae741f268856e
mahesh.thota31@gmail.com <N1@GitHub>
2019-12-12 18:47:43 +0530

    added base test and driver manager

--- src/main/java/util/ConfigFileReader.java
+++ src/main/java/util/ConfigFileReader.java
@@ -1,4 +1,24 @@
 package util;
 
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class ConfigFileReader {
+
+    private Properties properties;
+
+    public ConfigFileReader(){
+        FileInputStream fileInputStream;
+        properties = new Properties();
+        String configFilePath = System.getProperty("user.dir")+"/src/test/resources/config.properties";
+        try {
+            fileInputStream = new FileInputStream(configFilePath);
+            properties.load(fileInputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public String getConfigPropertyValue(String key){
+            return properties.getProperty(key);
+    }
 }
--- src/main/java/util/DriverManager.java
+++ src/main/java/util/DriverManager.java
@@ -1,4 +1,18 @@
 package util;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
 public class DriverManager {
+
+    public static WebDriver driver = null;
+    public DriverManager(){
+        ConfigFileReader configFileReader = new ConfigFileReader();
+        System.setProperty("webdriver.chrome.driver", "");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
+    }
 }
